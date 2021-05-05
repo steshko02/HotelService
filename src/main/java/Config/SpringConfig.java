c2604804bde4db01a33578d6c198b0f0c6762193
steshko02 <steshko02@mail.ru>
2021-05-05 22:44:56 +0300

    пп

---
+++ src/main/java/Config/SpringConfig.java
@@ -0,0 +1,10 @@
+package by.steshko.springlessons;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("by.steshko.springlessons")
+public class SpringConfig {
+}
